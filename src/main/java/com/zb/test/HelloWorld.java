4759a2a0450ddeb0e4bda12e4ce5cf3e104ebd60
zhangbo <18536828360@163.com>
2019-01-03 16:24:16 +0800

    first time add

---
+++ src/main/java/com/zb/test/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.zb.test;
+
+import org.junit.Test;
+
+public class HelloWorld {
+    @Test
+    public void helloWorld(){
+        System.out.println("hello world");
+        System.out.println("hello java");
+        System.out.println("hello maven");
+        System.out.println("hello SVN");
+    }
+
+    public static void main(String[] args) {
+    	System.out.println("123");
+    }
+}
